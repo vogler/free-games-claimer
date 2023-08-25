516d7ddc83a5414214cac5111fc2701a37585865
Jordy Mendoza <jordyamc@hotmail.com>
2023-08-25 15:02:10 -0600

    -Fix apprise

--- src/util.js
+++ src/util.js
@@ -135,4 +135,4 @@ export const notify = html => new Promise((resolve, reject) => {
 
 export const escapeHtml = unsafe => unsafe.replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll('\'', '&#039;');
 
-export const html_game_list = games => games.map(g => `- <a href="${g.url}">${escapeHtml(g.title)}</a> (${g.status})`).join('<br>');
+export const html_game_list = games => games.map(g => `- <a href='${g.url}'>${escapeHtml(g.title)}</a> (${g.status})`).join('<br>');
