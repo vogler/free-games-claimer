621032e459df85beb2996fa69ff93e8aeb526b9a
Ralf Vogler <ralf.vogler@gmail.com>
2023-08-30 19:20:28 +0200

    eg: notify about captcha before claim, closes #191

--- epic-games.js
+++ epic-games.js
@@ -204,6 +204,7 @@ try {
         captcha.waitFor().then(async () => { // don't await, since element may not be shown
           // console.info('  Got hcaptcha challenge! NopeCHA extension will likely solve it.')
           console.error('  Got hcaptcha challenge! Lost trust due to too many login attempts? You can solve the captcha in the browser or get a new IP address.')
+          await notify('epic-games: got captcha challenge right before claim. Use VNC to solve it manually.')
           // await page.waitForTimeout(2000);
           // const p = path.resolve(cfg.dir.screenshots, 'epic-games', 'captcha', `${filenamify(datetime())}.png`);
           // await captcha.screenshot({ path: p });
