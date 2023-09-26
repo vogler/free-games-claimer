0040d9d96c699d515abe2ff69a5fdf81f32bee4f
4n4n4s <ojedinger@gmail.com>
2023-09-26 19:12:09 +0000

    Sonarqube support with ESLint

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  'env': {
+    'es2021': true,
+    'node': true,
+  },
+  "extends": "eslint:recommended",
+  'overrides': [
+    {
+      'env': {
+        'node': true,
+      },
+      'files': [
+        '.eslintrc.{js,cjs}',
+      ],
+      'parserOptions': {
+        'sourceType': 'script',
+      },
+    },
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'rules': {
+    'semi': 'error',
+  },
+};
