a9e20928eec5b6ec8aee77cc2377be5c7f4feb82
Ralf Vogler <ralf.vogler@gmail.com>
2023-12-26 18:56:36 +0100

    pg: fix util.js -> src/util.js
    https://github.com/vogler/free-games-claimer/commit/64676795d144bef79a10700cde20ced5a267a4bd#r135743209

--- prime-gaming.js
+++ prime-gaming.js
@@ -1,7 +1,7 @@
 import { firefox } from 'playwright-firefox'; // stealth plugin needs no outdated playwright-extra
 import { authenticator } from 'otplib';
 import chalk from 'chalk';
-import { resolve, jsonDb, datetime, stealth, filenamify, prompt, confirm, notify, html_game_list, handleSIGINT } from './util.js';
+import { resolve, jsonDb, datetime, stealth, filenamify, prompt, confirm, notify, html_game_list, handleSIGINT } from './src/util.js';
 import { cfg } from './src/config.js';
 
 const screenshot = (...a) => resolve(cfg.dir.screenshots, 'prime-gaming', ...a);
